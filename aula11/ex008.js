9c6405008f1242ce7eb4a941c553286527e7e146
Isadora <isah3027@gmail.com>
2022-10-18 18:41:00 -0300

    Atualização Aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+//console.log("O console funcionou")
+
+var vel = 50
+
+console.log(`a velocidade do carro é ${vel}`)
+if (vel > 60) {
+  console.log(`você ultrapassou a velocidade permitida, você foi multado!`)
+}
+console.log(`dirija com sinto de segurança`)
